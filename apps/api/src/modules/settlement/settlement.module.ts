622906e33d28a35d76e2f9e06a3455e94fc7fd74
Rotichkiprono <kipronof57@gmail.com>
2026-09-19 11:16:03 +0300

    Task: add TokenizationModule import to SettlementModule

--- apps/api/src/modules/settlement/settlement.module.ts
+++ apps/api/src/modules/settlement/settlement.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { WebhookController } from './webhook.controller';
-import { WebhookService } from './webhook.service';
+import { Module } from "@nestjs/common";
+import { WebhookController } from "./webhook.controller";
+import { WebhookService } from "./webhook.service";
+import { TokenizationModule } from "../tokenization/tokenization.module";
 
 @Module({
+  imports: [TokenizationModule],
   controllers: [WebhookController],
   providers: [WebhookService],
   exports: [WebhookService],
