db8e916a3344ea055bc6a0664a2e42ef97d4530a
Rotichkiprono <kipronof57@gmail.com>
2026-09-19 04:46:08 +0300

    refactor: remove unused files and update RPC URLs in project configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // Add the rawBody configuration here
   const app = await NestFactory.create(AppModule, { rawBody: true });
-  const configService = app.get(ConfigService);
   
-  // MUST dynamically bind to the PORT environment variable provided by Cloud Run
+  const configService = app.get(ConfigService);
   const port = process.env.PORT || configService.get<number>('port') || 8080;
   
   app.setGlobalPrefix('api/v1');
-  await app.listen(port, '0.0.0.0'); // MUST bind to 0.0.0.0 in Cloud Run, not localhost
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
